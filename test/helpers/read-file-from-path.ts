126d339a0256383d07a696dea25d609bfea7458f
Max <max.finke@gmail.com>
2021-06-15 10:04:44 +0000

    Inline response for test

--- test/helpers/read-file-from-path.ts
+++
@@ -1,5 +0,0 @@
-import { readFile } from 'fs/promises';
-import path from 'path';
-
-export const readFileFromPath = (pathName: string) =>
-  readFile(path.resolve(__dirname, '../..', pathName), 'utf8');
--- test/utils/get-response.test.ts
+++ test/utils/get-response.test.ts
@@ -1,10 +1,41 @@
 import { getResponse } from '../../src/utils/get-response';
 import { fromXml } from '../../src/utils/send-request';
-import { readFileFromPath } from '../helpers/read-file-from-path';
 
-test('getResponse', async () => {
-  const response = getResponse(
-    fromXml(await readFileFromPath('test/fixtures/add-token-response.xml'))
-  );
+const RESPONSE = `<?xml version="1.0" encoding="UTF-8"?>
+<NABTransactMessage>
+    <MessageInfo>
+        <messageID>8af793f9af34bea0ecd7eff711c9d3</messageID>
+        <messageTimestamp>20140710115921873000+600</messageTimestamp>
+        <apiVersion>spxml-3.0</apiVersion>
+    </MessageInfo>
+    <RequestType>addToken</RequestType>
+    <MerchantInfo>
+        <merchantID>XYZ00</merchantID>
+    </MerchantInfo>
+    <Status>
+        <statusCode>0</statusCode>
+        <statusDescription>Normal</statusDescription>
+    </Status>
+    <Token>
+        <TokenList count="1">
+            <TokenItem ID="1">
+                <responseCode>00</responseCode>
+                <responseText>Successful</responseText>
+                <successful>yes</successful>
+                <tokenValue>1234123412341234</tokenValue>
+                <CreditCardInfo>
+                    <pan>444433XXXXXXX111</pan>
+                    <expiryDate>11/15</expiryDate>
+                    <cardType>6</cardType>
+                    <cardDescription>Visa</cardDescription>
+                </CreditCardInfo>
+                <amount>100</amount>
+                <transactionReference/> </TokenItem>
+        </TokenList>
+    </Token>
+</NABTransactMessage>`;
+
+test('getResponse', () => {
+  const response = getResponse(fromXml(RESPONSE));
   expect(response).toMatchSnapshot();
 });
